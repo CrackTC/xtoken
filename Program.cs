d684ef546d0aa90220120bda43333de3efc662be
CrackTC <chenrke369@gmail.com>
2024-01-14 08:03:42 +0800

    fix: resource leak

--- Program.cs
+++ Program.cs
@@ -194,16 +194,21 @@ internal static class XToken
                         context.Response.OutputStream.Close();
                         break;
                     }
-                    var text = Process.Start(new ProcessStartInfo
+
                     {
-                        FileName = "./xtoken",
-                        Arguments = "get",
-                        RedirectStandardOutput = true,
-                        UseShellExecute = false
-                    })?.StandardOutput.ReadToEnd() ?? "";
-                    Console.Error.WriteLine($"Response: {text}");
-                    context.Response.OutputStream.Write(Encoding.UTF8.GetBytes(text));
-                    context.Response.OutputStream.Close();
+                        string text;
+                        using var process = Process.Start(new ProcessStartInfo
+                        {
+                            FileName = "./xtoken",
+                            Arguments = "get",
+                            RedirectStandardOutput = true,
+                            UseShellExecute = false
+                        });
+                        text = process?.StandardOutput.ReadToEnd() ?? "";
+                        Console.Error.WriteLine($"Response: {text}");
+                        context.Response.OutputStream.Write(Encoding.UTF8.GetBytes(text));
+                        context.Response.OutputStream.Close();
+                    }
                     break;
                 default:
                     context.Response.StatusCode = 404;
